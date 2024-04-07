4a80a51fbd7dafa1542e9bbe07f779e36183a73e
HegedusAlexandra <alexandra.hegedus@vamosoft.hu>
2024-04-07 22:05:59 +0200

    band name genrator

---
+++ band_name_generator.py
@@ -0,0 +1,5 @@
+
+name = input('What is your name? ')
+print(f'Hellooo {name} my friend !')
+print(f'{name} You looks younger than {input('How old are you?')}')
+print(f"{len(name)} {input('Which city you grow up in?')} {input('What is your pet name?')}" )
